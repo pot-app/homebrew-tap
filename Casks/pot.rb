ce672cb72988f9711078bb725c13645cc8354f7d
Pylogmon <pylogmon@outlook.com>
2023-07-21 17:27:51 +0800

    pot 1.10.0 (#12)
    Co-authored-by: pot-app <support@pot-app.com>

--- Casks/pot.rb
+++ Casks/pot.rb
@@ -1,9 +1,9 @@
 cask "pot" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.9.1"
-  sha256 arm:   "63ab8213ef75fc83e5ab4896e433a8b9fde986f4482538ba084125a586867158",
-         intel: "e2cef986bc2f9abc74ed26922dbca69711b2c39b1ff0f9a8da3a19ad7382df36"
+  version "1.10.0"
+  sha256 arm:   "3cd7ef0908208259daca5cb8b0e0f9e82216a9014a8a2c90a91e362f976f23ac",
+         intel: "3cc0dd33071c3fc931962fbd454e093c6bb5093418d87a4018f7da8cf07c5691"
 
   url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
   name "pot"
