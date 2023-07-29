4b3924c6aa9b6278702798bfcccf1c61e08361b7
mrchi <i@mrchi.cc>
2023-07-29 15:38:01 +0800

    pot 1.12.0 (#13)

--- Casks/pot.rb
+++ Casks/pot.rb
@@ -1,9 +1,9 @@
 cask "pot" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.11.1"
-  sha256 arm:   "e33c69d1b365d469e5278db9395e44693432d3a6b7ee104ddc0d89850b8709a6",
-         intel: "c32511fa7e492ebb749f6829f9c775c8955430b7207da2170afb2ccfec0c113a"
+  version "1.12.0"
+  sha256 arm:   "d1abdaecf4cc2d34e1e453644ca95b3d44a1649d02ab9529b29e6e3eb1ca162f",
+         intel: "8c6f1c23b35249489122060563834e3f0327ea194440c89abed958daa2146ac1"
 
   url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
   name "pot"
