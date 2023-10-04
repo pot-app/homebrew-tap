eeee110d451632716c98f987d69b87f96076b92c
pot-app <support@pot-app.com>
2023-10-04 14:06:33 +0000

    pot 2.6.1

--- Casks/pot.rb
+++ Casks/pot.rb
@@ -1,9 +1,9 @@
 cask "pot" do
   arch arm: "aarch64", intel: "x64"
 
-  version "2.6.0"
-  sha256 arm:   "f0a60e7200c534b3d08f66bd045a616e943f3fcb1fda7e533769b84d8b895022",
-         intel: "0e5e697febd18678b8639f78121d27d0a9b1d20cff97975f263acea32bfb8da3"
+  version "2.6.1"
+  sha256 arm:   "fd76764219e0fd0d7624c7d136eafb8f420e531d4e697c8d03a8b050b6ba9d4e",
+         intel: "ec28aad770922a1c0ba571d84263b5caaa4b97f9c10593bab87f9c0b46b07c25"
 
   url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
   name "pot"
